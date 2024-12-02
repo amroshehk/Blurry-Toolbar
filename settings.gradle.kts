6559ea383efb6d6bfeef73f0db7b9fe1fd40730b
Amro Shekh Zain <amroshehzen@hotmail.com>
2024-12-02 20:35:41 +0200

    Blurry Toolbar
    You can achieve a blurry toolbar depending on a simple library like: RealtimeBlurView

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        maven(url = "https://maven.google.com")
+        maven(url = "https://jitpack.io")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://dl.cloudsmith.io/public/cometchat/cometchat-pro-android/maven/")
+    }
+}
+
+rootProject.name = "Blurry Toolbar"
+include(":app")
